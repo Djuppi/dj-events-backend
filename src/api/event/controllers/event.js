9131e773e7bf298d25bfc233d9a34c283f04d25b
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-14 13:34:45 +0100

    Added additional logic for /events/me and populate

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,4 +1,7 @@
+// PATH: ./src/api/[content-type]/controllers/[content-type].js
+
 "use strict";
+
  
 /**
  *  event controller
@@ -6,4 +9,31 @@
  
 const { createCoreController } = require("@strapi/strapi").factories;
  
-module.exports = createCoreController("api::event.event");
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+    async me(ctx) {
+      const user = await strapi.plugins[
+        'users-permissions'
+      ].services.jwt.getToken(ctx);
+
+      if(!user) {
+        return ctx.badRequest(null, [{messages: [{ id: "No authorization header was found" }]}]);
+      }
+
+      const data = await strapi.plugins['users-permissions'].services.user.fetchAuthenticatedUser(user.id);
+
+      if(!data.events) {
+        ctx.notFound();
+      }
+
+      return {user: { username: data.username, email: data.email }, events: data.events};
+    },
+    async find(ctx) {
+      ctx.query = { ...ctx.query, populate: '*' }
+    
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+
+  
+      return { data, meta };
+    }
+}));
