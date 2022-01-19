17757b12d196f342d1e35b650b1032019cda0a60
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-19 11:10:06 +0100

    Edit middlewares

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,35 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+      config: {
+        headers: '*',
+        origin: [
+          "https://djuppidjevntsbackend.herokuapp.com/",
+          "https://www.localhost:3000",
+          "https://localhost:1337"
+        ],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
+      }
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
+];
