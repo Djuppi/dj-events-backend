a072eed317c7122e57e3a5f3e221d95ef6afd881
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-19 13:26:09 +0100

    Editted prod middleware

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,35 +1,9 @@
-module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true, 
+      // headers: '*', 
+      origin: ["http://localhost", 'https://djuppidjevntsbackend.herokuapp.com'],
     },
   },
-  {
-    name: 'strapi::cors',
-      config: {
-        headers: ['Access-Control-Allow-Origin'],
-        origin: [
-          "https://djuppidjevntsbackend.herokuapp.com/*",
-          "https://www.localhost:3000",
-          "https://localhost:1337"
-        ],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
-      }
-  },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
-];
+}
