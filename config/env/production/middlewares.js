c746554fce90386706b46ed1f52745e70fff88cf
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-01-21 09:53:00 +0100

    Added frontend origin

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -3,7 +3,7 @@ module.exports = {
     cors: {
       enabled: true, 
       // headers: '*', 
-      origin: ["http://localhost:1337", "https://djuppidjevntsbackend.herokuapp.com"],
+      origin: ["http://localhost:1337", "https://djuppidjevntsbackend.herokuapp.com", "htts://djevents-ochre.app"],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
     },
   },
